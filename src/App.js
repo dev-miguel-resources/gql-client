778cf178c01b46d4d2eee5f0f66b5b9f3fd2e654
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-06-17 08:41:14 -0400

    integration apollo client with apollo server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from "apollo-boost";
+import ApolloProvider from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
